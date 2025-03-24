504cef4c7cc9b9e8c280f4222272ae6a81250500
FacundoDayne <operavgxgamer@gmail.com>
2025-03-25 01:12:55 +0800

    working on the requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
-
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    
 setup(
     name="FileWhisperer",
     version="1.0",
     packages=find_packages(),
-    install_requires=[
-        "pytaglib",
-        "python-magic-bin",
-        "pillow",
-        "git+https://github.com/OperavonderVollmer/OperaPowerRelay.git@v1.1.2",
-    ],
+    install_requires=requirements,
     python_requires=">=3.7",
     author="Opera von der Vollmer",
     description="Script for manipulating metadata of files",
